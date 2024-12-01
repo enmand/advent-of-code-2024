5f961b113b0e33fa1e12b20365f79ed2b165bbf6
Dan Enman <enmand@gmail.com>
2024-12-01 10:15:15 -0400

    chore: initial commit

---
+++ advent/src/lib.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::ops::Deref;
+
+pub fn input_from_args() -> std::io::Lines<BufReader<File>> {
+    let args = env::args().collect::<Vec<String>>();
+    let input = args_file(args, 1);
+
+    let f = File::open(input).expect("file not found");
+    BufReader::new(f).lines()
+}
+
+pub fn args_file(args: Vec<String>, skip: usize) -> String {
+    let input = args.get(skip).expect("No input file provided").deref();
+
+    if input == "." {
+        args_file(args, 2)
+    } else {
+        input.into()
+    }
+}
